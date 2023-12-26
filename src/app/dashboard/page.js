d821444b8986f31d40431b76695115bef623d548
manthan gadhiya <manthan.gadhiya.thinketic@gmail.com>
2023-12-26 11:03:36 +0530

    adding file

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Explore all available listings</h1>
+    </>
+  )
+}
+
+export default Home;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +11,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;600;700;800&display=swap" rel="stylesheet" />
+      </head>
+      <body
+        className={cn("relative h-full font-sans antialiased", inter.className)}
+      >
+        <main className="relative flex flex-col min-height-screen">
+          <div className="flex-grow flex-1">{children}</div>
+        </main>
+      </body>
     </html>
   )
 }
